9883a66830ed99900cc797501e1d94294f147d6e
Esteban <estebanyalg@gmail.com>
2023-05-29 13:28:19 -0500

    Change infrastructure

--- main.tf
+++ main.tf
@@ -2,75 +2,60 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_security_group" "instance_sg" {
-  name        = "Terraform-SG"
-  description = "Security group for EC2 instances"
+resource "aws_security_group" "jenkins_ecs_sg" {
+  name        = "JenkinsECS-Security"
+  description = "Security group for Jenkins ECS"
 
-  # Allow SSH access from HOST IP
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["186.114.47.119/32"]
-  }
-
-  # Allow outbound traffic to all destinations
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "agent_one" {
-  ami                    = "ami-024e6efaf93d85776"
-  instance_type          = "t2.micro"
-  key_name               = "worker"
-  security_groups        = [aws_security_group.instance_sg.id]
-  subnet_id              = "subnet-024051aa739d8f921"
-  count                  = 1
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-08333bccc35d71140"
+  instance_type = "t2.micro"
+  key_name      = "jenkins-master" #Key ID
+  vpc_security_group_ids = [aws_security_group.jenkins_ecs_sg.id]
+  subnet_id     = "subnet-01b134ee94ea26b30"
+
+  root_block_device {
+    volume_size = 8 #Disk size in GB
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "ECS_CLUSTER=${aws_ecs_cluster.jenkins_cluster.id}" >> /etc/ecs/ecs.config
+    echo "ECS_ENABLE_TASK_IAM_ROLE=true" >> /etc/ecs/ecs.config
+    echo "ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true" >> /etc/ecs/ecs.config
+
+    start ecs
+  EOF
+
   tags = {
-    Name = "agent-onne"
+    Name = "Jenkins-EC2"
   }
 }
 
-resource "aws_instance" "agent_two" {
-  ami                    = "ami-024e6efaf93d85776"
-  instance_type          = "t2.micro"
-  key_name               = "worker"
-  security_groups        = [aws_security_group.instance_sg.id]
-  subnet_id              = "subnet-024051aa739d8f921"
-  count                  = 1
-  tags = {
-    Name = "agent-two"
-  }
-}
-
-resource "aws_instance" "go_server" {
-  ami                    = "ami-024e6efaf93d85776"
-  instance_type          = "t2.micro"
-  key_name               = "worker"
-  security_groups        = [aws_security_group.instance_sg.id]
-  subnet_id              = "subnet-024051aa739d8f921"
-  count                  = 1
-  tags = {
-    Name = "Go-Server"
-  }
-}
-
-resource "aws_iam_role" "terraform_admin" {
-  name = "TerraformAdmin"
-
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-UniqueName" #Unique name for the IAM role
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "",
       "Effect": "Allow",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "ecs-tasks.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -79,7 +64,34 @@ resource "aws_iam_role" "terraform_admin" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "terraform_admin_policy_attachment" {
-  role       = aws_iam_role.terraform_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_ecs_task_definition" "jenkins_task" {
+  family                   = "jenkins-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "bridge"
+
+  requires_compatibilities = ["EC2"]
+
+  cpu                      = 1024
+  memory                   = 2048 #Memory in MB
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "jenkins-container",
+      "image": "jenkins/jenkins:lts-jdk11",
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080,
+          "protocol": "tcp"
+        }
+      ],
+      "essential": true
+    }
+  ]
+  DEFINITION
+}
+
+resource "aws_ecs_cluster" "jenkins_cluster" {
+  name = "jenkins-cluster"
 }
